29820f13d1b9c61dc6eb4eaf765861507d83e45e
Haoming Li <61410145+haoming-codes@users.noreply.github.com>
2025-10-27 11:22:22 -0700

    Add simulated streaming transcription CLI

--- whisperlivekit/__init__.py
+++ whisperlivekit/__init__.py
@@ -1,11 +1,12 @@
 from .audio_processor import AudioProcessor
 from .core import TranscriptionEngine
-from .parse_args import parse_args
+from .parse_args import build_argument_parser, parse_args
 from .web.web_interface import get_web_interface_html, get_inline_ui_html
 
 __all__ = [
     "TranscriptionEngine",
     "AudioProcessor",
+    "build_argument_parser",
     "parse_args",
     "get_web_interface_html",
     "get_inline_ui_html",
